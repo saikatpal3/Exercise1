acbcafc499cd40c25faafedc3a7ea7435265174a
“saikatpal3” <saikatpal3@gmail.com>
2021-02-02 14:18:18 -0500

    ..Exercise1

---
+++ src/main/java/com/amazon/pom/pages/HomePage.java
@@ -0,0 +1,37 @@
+package com.amazon.pom.pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import com.amazon.pom.base.BaseTest;
+
+public class HomePage extends BaseTest {
+	
+	@FindBy(xpath = "//input[@id='twotabsearchtextbox']")
+	WebElement searchBox;
+	
+	@FindBy(xpath = "//input[@id='nav-search-submit-button']")
+	WebElement submitButton;
+	
+	@FindBy(xpath = "//select[@id='searchDropdownBox']")
+	WebElement selectBooks;
+	
+	public HomePage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public SearchPage performSearch(){
+		//searchBox.sendKeys("qa testing for beginners");
+		//submitButton.click();
+		Select select = new Select(selectBooks);
+		select.selectByVisibleText("Books");
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("arguments[0].value='qa testing for beginners';", searchBox);
+		jse.executeScript("arguments[0].click();", submitButton);
+		return new SearchPage();
+	}
+
+}
